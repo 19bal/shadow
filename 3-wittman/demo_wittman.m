f8f151afe3f129186768399053ee62e6b75994f6
Nurettin Senyer <seyyah@bil.omu.edu.tr>
2010-11-05 13:46:54 +0200

    shadow_detection yeterli degil; yeni yonteme geciliyor

--- 3-wittman/demo_wittman.m
+++ 3-wittman/demo_wittman.m
@@ -5,13 +5,15 @@ DIR = dir(strcat(dbnm, '*.png'));
 
 sz = length(DIR);
 
-for f=1:sz
+while(1)
+for f=[1:140 155:250]%sz
     imgnm = DIR(f).name;
     cfrm = imread(strcat(dbnm, imgnm)); 
     
     figure(1),  
-    subplot(221),   imshow(cfrm);
+    subplot(221),   imshow(cfrm);   title(num2str(f));
     subplot(222),   imshow(shadow_detection(cfrm));
-    subplot(223),   imshow(shadow_remove(cfrm, 3));
+    %subplot(223),   imshow(shadow_remove(cfrm, 3));
     drawnow;
+end
 end
