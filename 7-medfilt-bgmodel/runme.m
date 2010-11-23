cb8de11d17c12d8510c8445623cb65d6cf25b766
Nurettin Senyer <seyyah@bil.omu.edu.tr>
2010-11-23 15:09:55 +0200

    lib tasarimina geciliyor

--- 7-medfilt-bgmodel/runme.m
+++ 7-medfilt-bgmodel/runme.m
@@ -1,13 +1,22 @@
-dbnm = '../../../db/surveillance/';
+%%%%%%%%%%%%%%%% D O   N O T   E D I T   M E%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+LIB_PATH = sprintf('..%slib%s', filesep,filesep);                         %
+addpath(LIB_PATH,'-end');                                                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+dbnm = pathos(strcat(DB_ROOT(), 'gait/surveillance/'));
 DIR = dir(strcat(dbnm, '*.png'));
 dbg = true;
 
-DIR = DIR(1:150);
+DIR = DIR(1:200);
 
 % bg-model
 bg_med = bg_color(DIR, dbnm, dbg);
+save bg_med.mat bg_med
 
-T = 50;
+load bg_med
+
+T = 30;
+mc = 25;    Mc = 45;
 sz = length(DIR);
 
 for f = 1:sz,
@@ -17,7 +26,11 @@ for f = 1:sz,
     fark = uint8(abs(double(cfrm) - double(bg_med)));
     bw = (fark > T);
     
+    confC = frm2confC(cfrm, bg_med, mc, Mc, T, false);
+    
+    
     figure(1);
-    imshow(uint8(255*bw))
+        subplot(121),   imshow(cfrm)
+        subplot(122),   imshow(confC)
     drawnow;
 end
