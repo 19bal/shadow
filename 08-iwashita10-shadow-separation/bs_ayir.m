a7af04d8cc52effd0b8957edae7f9467f5a556cb
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-12-16 09:27:33 +0200

    hala calisiyoruz ...

--- 08-iwashita10-shadow-separation/bs_ayir.m
+++ 08-iwashita10-shadow-separation/bs_ayir.m
@@ -1,4 +1,4 @@
-function [bwb, bws] = bs_ayir(bw, dbg)
+% function [bwb, bws] = bs_ayir(bw, dbg)
 % function [bwb, bws] = bs_ayir(bw, dbg)
 
 % body
@@ -7,12 +7,38 @@ function [bwb, bws] = bs_ayir(bw, dbg)
 [sr, si] = sort(r);
 sc = c(si);
 
+% x - eksenindeki degisim
+tmp = sc - min(sc);
+id = find(tmp < 2);
+tmp(id) = 0;
+
+% kirilma noktasi: body-shadow
+% tmp in '0' dan farklandigi "ilk" yerdir
+id = find(tmp);
+id = id(1) - 1;
+
+% koordinat karsiliklari
+ky = sr(id);
+kx = sc(id);
+
+if dbg    
+    figure(31),
+    plot(tmp);
+    hold on
+    plot(id,tmp(id), 'r*')
+    hold off
+    title('x-yonundeki degisim')
+    xlabel('id');   ylabel('degisim')
+    grid on
+end
+
+return
 p = [sr sc];
 
 fark = abs(p(1:end-1, :) - p(2:end, :));
 
 fark2 = fark - repmat([1 0], length(fark), 1);
-fark3 = fark2(:,1) + fark2(:,2);
+fark3 = abs(fark2(:,1)) + abs(fark2(:,2));
 
 id = find(fark3 == 0);
 
@@ -30,4 +56,13 @@ bwb = bwareaopen(bwb, 5);
 % shadow
 bws = boolean(bw - bwb);
 bws = bwareaopen(bws, 5);
+
+if dbg
+    figure(32)
+    subplot(221),   imshow(bw), title('bw')
+    subplot(222),   imshow(bw), title('')
+    subplot(223),   imshow(bwb), title('bwb')
+    subplot(224),   imshow(bws), title('bws')
+end
+
 
