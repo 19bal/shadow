b097372be7375662cd5bd42e5efd9399012fd5c2
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-12-16 07:54:38 +0200

    iscreate* notasyonuna gecildi

--- 07-medfilt-bgmodel/runme.m
+++ 07-medfilt-bgmodel/runme.m
@@ -12,13 +12,18 @@ dbnm_siluet = pathos('_db/siluet/');    mkdir(dbnm_siluet);
 dbnm_64x64  = pathos('_db/64x64/');     mkdir(dbnm_64x64);
 dbnm_bkp = pathos('_bkp/');             mkdir(dbnm_bkp);
 
-if ~exist((pathos(strcat(dbnm_bkp, 'bg_model.png')))),
+iscreate_bg     = ~exist((pathos(strcat(dbnm_bkp, 'bg_model.png'))));
+iscreate_bw     = length(dir(strcat(dbnm_bw, '*.png'))) < 1;
+iscreate_siluet = length(dir(strcat(dbnm_siluet, '*.png'))) < 1;
+iscreate_64x64  = length(dir(strcat(dbnm_64x64, '*.png'))) < 1;
+
+if iscreate_bg
     bg = bg_model(dbnm, dbnm_bkp, 200, dbg);
 else
     bg = imread(pathos(strcat(dbnm_bkp, 'bg_model.png')));
 end
 
-if length(dir(strcat(dbnm_bw, '*.png'))) < 1
+if iscreate_bw
     dbnm_bw = frm2bw_db(dbnm, dbnm_bw, bg, dbg);
 end
 
@@ -26,7 +31,7 @@ DIR = dir(strcat(dbnm, '*.png'));
 DIR_bw = dir(strcat(dbnm_bw, '*.png'));
 sz = length(DIR_bw);
 
-for f = 1:135,
+for f = 100%1:135,
     fprintf('kare %04d/%04d isleniyor ...\n', f, sz);
 
     imgnm = DIR(f).name;    
@@ -35,16 +40,16 @@ for f = 1:135,
     imgnm_bw = DIR_bw(f).name;    
     bw = double(imread(strcat(dbnm_bw, imgnm_bw)));
     
-    %if length(dir(strcat(dbnm_siluet, '*.png'))) < 1
-        bwr = insanlar(bw, false);
+    if iscreate_siluet
+        bwr = insanlar(bw, dbg);
         imwrite(bwr, strcat(dbnm_siluet, imgnm));
-    %else
-    %    bwr = imread(strcat(dbnm_siluet, imgnm));
-    %end
+    else
+        bwr = imread(strcat(dbnm_siluet, imgnm));
+    end
     
     frh = fr .* uint8(cat(3, bwr,bwr,bwr));
     
-    if length(dir(strcat(dbnm_64x64, '*.png'))) < 1
+    if iscreate_64x64
         bw64x64 = bwsresize(bwscrop(bw2silh(bwr)));
         imwrite(bw64x64, strcat(dbnm_64x64, imgnm));
     else
