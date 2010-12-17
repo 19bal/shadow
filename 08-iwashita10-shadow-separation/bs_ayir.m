b43fae81f4e163ffb6899c595b81ec7236448ac4
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-12-17 06:21:30 +0200

    calisan versiyonu:OK

--- 08-iwashita10-shadow-separation/bs_ayir.m
+++ 08-iwashita10-shadow-separation/bs_ayir.m
@@ -1,5 +1,5 @@
-% function [bwb, bws] = bs_ayir(bw, dbg)
-% function [bwb, bws] = bs_ayir(bw, dbg)
+function [bwb, bws, ky] = bs_ayir(bw, dbg)
+% function [bwb, bws, ky] = bs_ayir(bw, dbg)
 
 % body
 [r, c] = find(bw);
@@ -8,61 +8,41 @@
 sc = c(si);
 
 % x - eksenindeki degisim
-tmp = sc - min(sc);
-id = find(tmp < 2);
-tmp(id) = 0;
+T = 10;
+degisim = sc - min(sc);
+id = find(degisim < T);
+
+degisim_esiklenmis = degisim;
+degisim_esiklenmis(id) = 0;
 
 % kirilma noktasi: body-shadow
 % tmp in '0' dan farklandigi "ilk" yerdir
-id = find(tmp);
-id = id(1) - 1;
-
-% koordinat karsiliklari
-ky = sr(id);
-kx = sc(id);
+id = find(degisim_esiklenmis);
+id = id(1) - T;
 
 if dbg    
     figure(31),
-    plot(tmp);
+    plot(degisim);
     hold on
-    plot(id,tmp(id), 'r*')
+    plot(id, degisim(id), 'r*')
     hold off
     title('x-yonundeki degisim')
     xlabel('id');   ylabel('degisim')
     grid on
+    text(id-4, 2, sprintf('sp:%d', id));
 end
 
-return
-p = [sr sc];
+% koordinat karsiliklari
+ky = sr(id);
 
-fark = abs(p(1:end-1, :) - p(2:end, :));
-
-fark2 = fark - repmat([1 0], length(fark), 1);
-fark3 = abs(fark2(:,1)) + abs(fark2(:,2));
-
-id = find(fark3 == 0);
-
-ri = r(si(id));
-ci = c(si(id));
-
-bwb = boolean(zeros(size(bw)));
-sz = length(ri);
-for i=1:sz
-    bwb(ri(i), ci(i)) = true;
-end
-
-bwb = bwareaopen(bwb, 5);
-
-% shadow
-bws = boolean(bw - bwb);
-bws = bwareaopen(bws, 5);
+bwb = bw(1:ky-1, :);
+bws = bw(ky:end, :);
 
 if dbg
     figure(32)
     subplot(221),   imshow(bw), title('bw')
-    subplot(222),   imshow(bw), title('')
+ 
     subplot(223),   imshow(bwb), title('bwb')
     subplot(224),   imshow(bws), title('bws')
 end
-
 
